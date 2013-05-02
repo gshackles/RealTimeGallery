ed2663aa192e4f07539b00de33fd38c5bf703b0d
Greg Shackles <gshackles@gmail.com>
2013-05-02 00:47:24 -0400

    making some updates to the iOS app

--- GalleryApp-iOS/MainViewController.cs
+++ GalleryApp-iOS/MainViewController.cs
@@ -1,28 +1,38 @@
 using System;
 using System.Runtime.InteropServices;
+using GalleryApp.Core;
 using MonoTouch.Dialog;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
-using GalleryApp.Core;
 
 namespace GalleryAppiOS
 {
     public partial class MainViewController : DialogViewController
     {
-        private UIImagePickerController _picker;
         private const string UploadUrl = "http://192.168.1.103/api/photo";
-        private readonly PhotoUploader _uploader;
-        private readonly PhotoListener _listener;
 
-        public MainViewController() : base (UITableViewStyle.Grouped, null)
+        private UIImagePickerController _picker;
+        private PhotoUploader _uploader;
+        private PhotoListener _listener;
+        private Section _imageSection;
+
+        public MainViewController() 
+            : base (UITableViewStyle.Grouped, null)
         {
+            _imageSection = new Section();
+
             Root = new RootElement("RealTime Gallery")
             {
-                new Section ()
-                {
-                    new StringElement("Upload a picture", UploadPicture)
-				}
+                _imageSection
             };
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            NavigationItem.RightBarButtonItem = new UIBarButtonItem(UIBarButtonSystemItem.Add);
+            NavigationItem.RightBarButtonItem.Clicked += delegate { UploadPicture(); };
 
             _uploader = new PhotoUploader();
             _listener = new PhotoListener();
@@ -32,14 +42,14 @@ namespace GalleryAppiOS
                 InvokeOnMainThread(() =>
                 {
                     foreach (var url in urls)
-                        new UIAlertView("New Photo", url, null, "Ok", null).Show();
+                    {
+                        _imageSection.Add(
+                            new ImageStringElement(DateTime.Now.ToString(), 
+                                                   UIImage.LoadFromData(NSData.FromUrl(new NSUrl(url))))
+                        );
+                    }
                 });
             };
-        }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
 
             _listener.StartListening();
         }
@@ -48,6 +58,7 @@ namespace GalleryAppiOS
         {
             _picker = new UIImagePickerController();
             _picker.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            _picker.Canceled += delegate { _picker.DismissViewController(true, null); };
             _picker.FinishedPickingMedia += (s, e) =>
             {
                 _picker.DismissViewController(true, null);
